f1b30ae9c3900df3a7f2521655b06a2cfeb337ec
atik-2005 <diwanatik529@gmail.com>
2026-08-12 11:48:16 +0530

    Replace KeyBERT with lightweight keyword extraction

--- app/ai_engine/keyword_extractor.py
+++ app/ai_engine/keyword_extractor.py
@@ -1,28 +1,32 @@
-import re
 from collections import Counter
-
-
-STOP_WORDS = {
-    "the", "a", "an", "and", "or", "but", "if", "then",
-    "is", "are", "was", "were", "be", "been", "being",
-    "to", "of", "in", "on", "for", "with", "as", "by",
-    "at", "from", "this", "that", "these", "those",
-    "it", "its", "into", "about", "than", "their",
-    "they", "them", "he", "she", "his", "her",
-    "you", "your", "we", "our", "can", "will"
-}
+import re
 
 
 def extract_keywords(text, top_n=20):
+    """
+    Lightweight keyword extraction.
+    No KeyBERT, PyTorch, or Transformer model required.
+    """
 
-    words = re.findall(r"\b[a-zA-Z]{3,}\b", text.lower())
+    words = re.findall(r"\b[a-zA-Z][a-zA-Z0-9-]{2,}\b", text.lower())
 
-    words = [
+    stop_words = {
+        "the", "and", "for", "are", "with", "that", "this",
+        "from", "was", "were", "have", "has", "had", "not",
+        "but", "you", "your", "their", "they", "there",
+        "what", "which", "when", "where", "how", "why",
+        "into", "about", "then", "than", "also", "can",
+        "will", "would", "could", "should", "been", "being",
+        "using", "used", "use", "its", "it", "is", "in",
+        "on", "of", "to", "a", "an", "as", "by", "or"
+    }
+
+    filtered_words = [
         word for word in words
-        if word not in STOP_WORDS
+        if word not in stop_words
     ]
 
-    frequency = Counter(words)
+    frequency = Counter(filtered_words)
 
     return [
         word
