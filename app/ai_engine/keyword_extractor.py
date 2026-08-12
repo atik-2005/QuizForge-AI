6644fb361d1b3a760ee30605018a5a029af39a5f
atik-2005 <diwanatik529@gmail.com>
2026-08-12 12:03:20 +0530

    Remove heavy KeyBERT dependencies

--- app/ai_engine/keyword_extractor.py
+++ app/ai_engine/keyword_extractor.py
@@ -2,31 +2,25 @@ from collections import Counter
 import re
 
 
+STOP_WORDS = {
+    "the", "is", "are", "was", "were", "a", "an", "and", "or",
+    "of", "to", "in", "on", "for", "with", "by", "from", "as",
+    "at", "this", "that", "these", "those", "it", "its", "be",
+    "been", "being", "has", "have", "had", "can", "will", "would",
+    "should", "could", "about", "into", "than", "then", "their",
+    "there", "they", "them", "we", "you", "your", "he", "she",
+    "his", "her"
+}
+
+
 def extract_keywords(text, top_n=20):
-    words = re.findall(
-        r"\b[a-zA-Z][a-zA-Z0-9-]{2,}\b",
-        text.lower()
-    )
+    words = re.findall(r"\b[a-zA-Z]{3,}\b", text.lower())
 
-    stop_words = {
-        "the", "and", "for", "are", "with", "that", "this",
-        "from", "was", "were", "have", "has", "had", "not",
-        "but", "you", "your", "their", "they", "there",
-        "what", "which", "when", "where", "how", "why",
-        "into", "about", "then", "than", "also", "can",
-        "will", "would", "could", "should", "been", "being",
-        "using", "used", "use", "its", "it", "is", "in",
-        "on", "of", "to", "a", "an", "as", "by", "or"
-    }
-
-    filtered_words = [
+    words = [
         word for word in words
-        if word not in stop_words
+        if word not in STOP_WORDS
     ]
 
-    frequency = Counter(filtered_words)
+    frequency = Counter(words)
 
-    return [
-        word
-        for word, count in frequency.most_common(top_n)
-    ]
+    return [word for word, count in frequency.most_common(top_n)]
